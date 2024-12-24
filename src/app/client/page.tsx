b92bff19b9cbe0bcd295439de3cc50ab435316e1
YALIM GÜRBÜZ <99664429+ylmgrbzz@users.noreply.github.com>
2024-12-24 20:12:36 +0300

    Add ClientPage component with session management and UserCard display

---
+++ src/app/client/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import UserCard from "../components/UserCard";
+
+export default function ClientPage() {
+  //const { data: session } = useSession();
+
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/client");
+    },
+  });
+
+  console.log("ClientPage", session);
+
+  if (!session) {
+    redirect("/api/auth/signin");
+    return null;
+  }
+
+  return (
+    <section className="flex flex-col gap-6">
+      <UserCard user={session?.user} pagetype={"Client"} />
+    </section>
+  );
+}
